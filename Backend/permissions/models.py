4ccb2cf255592f5d247929ea777802e9079abaf4
Syed Jawad Ali <syedjawadali92@gmail.com>
2024-12-21 22:35:30 +0500

    first commit

---
+++ Backend/permissions/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# from  e_commerce.settings import AUTH_USER_MODEL
+
+# Create your models here.
+
+class Role(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    code = models.CharField(max_length=50, null=True, blank=True)
+    permissions = models.ManyToManyField('Permission', related_name='roles')
+
+
+class Permission(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    code = models.CharField(max_length=50)
+    module_name = models.CharField(max_length=50, null=True, blank=True)
+
+
