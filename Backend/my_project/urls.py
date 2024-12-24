f20fa3b025d5203b0df32aa51db5cfd59c83fc79
Syed Jawad Ali <syedjawadali92@gmail.com>
2024-12-24 21:54:10 +0500

    Changes

--- Backend/my_project/urls.py
+++ Backend/my_project/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user_auth.urls')),
     path('permission/', include('permissions.urls')),
+    path('images/', include('images_app.urls')),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
